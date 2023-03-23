4bd64b2cb7fc7e574516d9fbf6a916daf316397a
Rudi Wijaya <rudi.mail90@gmail.com>
2023-03-23 11:06:57 +0700

    writing markup with JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Gallery from './Gallery';
 import { Profile } from './Profile';
+import ToDoList from './ToDoList';
 
 function App() {
   return (
     <div>
       <Profile />
       <Gallery />
+      <ToDoList />
     </div>
   );
 }
