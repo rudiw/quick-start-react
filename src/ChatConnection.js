5d5d6d2825f232682502fee19e57e8fcff1e9f4d
Rudi Wijaya <rudi.mail90@gmail.com>
2023-04-19 13:42:42 +0700

    removing effect dependencies

--- src/ChatConnection.js
+++ src/ChatConnection.js
@@ -1,12 +1,31 @@
-export function createConnection(serverUrl, roomId) {
+export function createConnection({ serverUrl, roomId }) {
+  let connectedCallback;
+  let timeout;
   return {
     connect() {
+      timeout = setTimeout(() => {
+        if (connectedCallback) {
+          connectedCallback();
+        }
+      }, 2000);
+
       console.log(
         '✅ Connecting to "' + roomId + '" room at ' + serverUrl + '...'
       );
     },
+    on(event, callback) {
+      if (connectedCallback) {
+        throw Error('Cannot add the handler twice.');
+      }
+      if (event != 'connected') {
+        throw Error('Only "connected" event is supported.');
+      }
+
+      connectedCallback = callback;
+    },
     disconnect() {
-      console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl);
+      // console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl);
+      clearTimeout(timeout);
     },
   };
 }
--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,21 +1,36 @@
 import { useEffect, useState } from 'react';
 import { createConnection } from './ChatConnection';
+import { showNotification } from './Notification';
 
 const serverUrl = 'https://localhost:1234';
 
-function ChatRoom({ roomId }) {
+function ChatRoom({ roomId, theme }) {
+  const [message, setMessage] = useState('');
+
   useEffect(() => {
-    const con = createConnection(serverUrl, roomId);
+    const options = {
+      serverUrl: serverUrl,
+      roomId: roomId,
+    };
+    const con = createConnection(options);
+    con.on('connected', () => {
+      showNotification('Connected!', theme);
+    });
     con.connect();
     return () => con.disconnect();
-    // con.disconnect();
   }, [roomId]);
 
-  return <h1>Welcome to the {roomId} room!</h1>;
+  return (
+    <>
+      <h1>Welcome to the {roomId} room!</h1>
+      <input value={message} onChange={(e) => setMessage(e.target.value)} />
+    </>
+  );
 }
 
 export default function ChatRoomApp() {
   const [roomId, setRoomId] = useState('general');
+  const [isDark, setIsDark] = useState(false);
 
   return (
     <>
@@ -27,8 +42,16 @@ export default function ChatRoomApp() {
           <option value="music">music</option>
         </select>
       </label>
+      <label>
+        <input
+          type="checkbox"
+          checked={isDark}
+          onChange={(e) => setIsDark(e.target.checked)}
+        />
+        Use dark theme
+      </label>
       <hr />
-      <ChatRoom roomId={roomId} />
+      <ChatRoom roomId={roomId} theme={isDark ? 'dark' : 'light'} />
     </>
   );
 }
